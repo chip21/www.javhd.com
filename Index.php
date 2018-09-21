1b2d5562f2deb41cef6c0b380fa9c35c0bc6f083
chip21 <43479788+chip21@users.noreply.github.com>
2018-09-21 13:55:37 -0400

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,17 @@
+<title>Mr.Chip21 || UNMASKED</title><Center>
+	<link rel="icon"  type="img/gif" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQySbF6L3_Ulc2ldJbVIckKRoVKAsNwnhyG8IlJI6MwFHyyzfV2">
+ <style type="text/css"> body{ color: #fff; background-image: url("http://4.bp.blogspot.com/-IKUJQQCmBnk/T81nADuR4EI/AAAAAAAAE0Y/RLMVEbpoGm4/s1600/Black-wallpapers-9.jpg"); cursor: url(http://hellox.persiangig.com/DefacePage/negro.cur), progress; } body,td,th { /* [disabled]color: #000; */ } </style> <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'><br><br><br>
+<img width="300" src="https://preview.ibb.co/gGaZNc/20180223_011330.jpg">
+<BR>
+<font color="white" face="Amatic Sc" size="10" style="text-shadow: 1px 3px 4px #ff0000;">Mr.Chip21 || UNMASKED<font size="6"><br>
+Hidden Ghost Team<br><Br><font size="5">
+Got Hacked?!?!<Br>
+Testing System bruh!!<Br>
+[ NOT FOR CRIME ]<br>
+[ MAKE YOUR HAPPY TODAY ]<br>
+Don't forget > SAFETY NUMBER ONE ! <br>
+<br><br>Contact:
+chip210419@gmail.com<br><Br>
+Thanks To:<marquee width="20%">|| Mc'Slovv || Cy#b3r00t || Mr.An4rk! || Mr.xBaraKuda || Mr.Cakil || 4LM05TH3V!L || Mr.Mr Cvl || Temboxxxz || || 4RCH!VER || Mr.TRINITY || ZORLAX || TN72 || Cl4yZero || Anonimokerl || CINDO_PUNK || EppCrazy || CROPz || ./C3W3KBerB4T4n9 || All Hidden Ghost Team || All Idiot Blackhat team || All Sora cyber team || AND YOU ADMIN :) </marquee>
+<style type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Amatic SC' rel='stylesheet' type='text/css'>
